4201d712e832cb4cd7d6527b7d39c45398a4d18c
Masha <rew2794@gmail.com>
2021-05-07 17:09:19 +0300

    adaptation 1400, 1200, 991, 767

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+   const menu = document.querySelector('.menu__list'),
+      menuItem = document.querySelectorAll('.menu__item'),
+      hamburger = document.querySelector('.hamburger');
+
+   hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger-active');
+      menu.classList.toggle('menu__list-active');
+   });
+
+   menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+         hamburger.classList.toggle('hamburger-active');
+         menu.classList.toggle('menu__list-active');
+      })
+   })
+})
